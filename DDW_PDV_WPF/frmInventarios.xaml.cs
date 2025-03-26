3877dd37d5cac0287424683e0114737525be4434
Erik Ricardo Campos Rosales <erikr.campos@gmail.com>
2025-03-26 04:53:52 -0600

    Correcciones al agregado de nuevos productos
    Se corrigió el problema de las imágenes almacenadas en el proyecto

--- DDW_PDV_WPF/frmInventarios.xaml.cs
+++ DDW_PDV_WPF/frmInventarios.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DDW_PDV_WPF.Controlador;
+using DDW_PDV_WPF.Modelo;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -13,27 +17,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace DDW_PDV_WPF
 {
-   
 
-   
-    public partial class frmInventarios : Page
+
+
+    public partial class frmInventarios : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         private DispatcherTimer _timer;
+        private readonly ApiService _apiService;
+        private ObservableCollection<ArticuloDTO> _listaArticulos;
+        private ArticuloDTO _articuloSeleccionado;
+
+        public ObservableCollection<ArticuloDTO> ListaArticulos
+        {
+            get { return _listaArticulos; }
+            set
+            {
+                _listaArticulos = value;
+                OnPropertyChanged(nameof(ListaArticulos)); // Notificar cambios
+            }
+        }
+
+        public ArticuloDTO ArticuloSeleccionado
+        {
+            get { return _articuloSeleccionado; }
+            set
+            {
+                _articuloSeleccionado = value;
+                OnPropertyChanged(nameof(ArticuloSeleccionado)); // Notificar cambios
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public frmInventarios()
         {
             InitializeComponent();
+
             _timer = new DispatcherTimer();
             _timer.Interval = TimeSpan.FromSeconds(1); // Actualizar cada segundo
             _timer.Tick += Timer_Tick;
             _timer.Start();
 
+            // Inicializar ApiService
+            _apiService = new ApiService();
+            CargarDatos();
+            DataContext = this;
+
             // Actualizar la fecha y hora al iniciar
             UpdateDateTime();
         }
+
+
 
         private void Timer_Tick(object sender, EventArgs e)
         {
@@ -53,24 +97,137 @@ namespace DDW_PDV_WPF
             txtHora.Text = now.ToString("HH:mm:ss", new CultureInfo("es-MX"));
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void CargarDatos()
         {
+            var resultado = await _apiService.GetAsync<List<ArticuloDTO>>("api/CArticulos/");
 
+            if (resultado != null)
+            {
+                ListaArticulos = new ObservableCollection<ArticuloDTO>(resultado); // Asigna los datos a la propiedad
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void BotonAgregar(object sender, RoutedEventArgs e)
         {
 
+            ArticuloSeleccionado = new ArticuloDTO();
+            AgregarPopup.IsOpen = true;
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private async void BotonQuitar(object sender, RoutedEventArgs e)
+        {
+            if (ArticuloSeleccionado != null)
+            {
+                MessageBoxResult resultado = MessageBox.Show(
+                    $"¿Eliminar {ArticuloSeleccionado.Descripcion}?",
+                    "Confirmar eliminación",
+                    MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (resultado == MessageBoxResult.Yes)
+                {
+                    bool eliminado = await _apiService.DeleteAsync($"api/CArticulos/{ArticuloSeleccionado.idArticulo}");
+
+                    if (eliminado)
+                    {
+                        ListaArticulos.Remove(ArticuloSeleccionado);
+                        MessageBox.Show("Artículo eliminado correctamente.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al eliminar el artículo.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un artículo para eliminar.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private async void BotonGuardarPopup(object sender, RoutedEventArgs e)
+        {
+            if (ArticuloSeleccionado == null)
+            {
+                MessageBox.Show("No hay artículo seleccionado.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(ArticuloSeleccionado.Foto))
+            {
+                MessageBox.Show("Debe seleccionar una imagen.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            var resultado = await _apiService.PostAsync("api/CArticulos/", ArticuloSeleccionado);
+
+            if (resultado)  
+            {
+
+                ListaArticulos.Add(ArticuloSeleccionado);
+                MessageBox.Show("Artículo agregado exitosamente.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                AgregarPopup.IsOpen = false;
+            }
+            else
+            {
+
+                MessageBox.Show("Error al guardar el artículo.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+
+
+
+
+        private void BotonCancelarPopup(object sender, RoutedEventArgs e)
         {
 
+            AgregarPopup.IsOpen = false;
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void BtnSeleccionarImagen_Click(object sender, RoutedEventArgs e)
         {
 
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imágenes|*.jpg;*.jpeg;*.png;*.gif"; 
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string imagenSeleccionada = openFileDialog.FileName;
+
+
+                string destinoCarpeta = System.IO.Path.Combine(Directory.GetCurrentDirectory(),"Imagenes");
+
+
+                if (!Directory.Exists(destinoCarpeta))
+                {
+                    Directory.CreateDirectory(destinoCarpeta);
+                }
+
+
+                string nombreImagen =    System.IO.Path.GetFileName(imagenSeleccionada);
+                string rutaDestino = System.IO.Path.Combine(destinoCarpeta, nombreImagen);
+
+                try
+                {
+                    File.Copy(imagenSeleccionada, rutaDestino, true); 
+
+                    TxtImagenRuta.Text = System.IO.Path.Combine("Imagenes", nombreImagen);
+                    ArticuloSeleccionado.Foto = rutaDestino;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al guardar la imagen: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
+
+
+
+
     }
 }
