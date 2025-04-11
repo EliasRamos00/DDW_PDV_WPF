bc8064aaae30dc41986168532a4a12af1e43444f
Erik Ricardo Campos Rosales <erikr.campos@gmail.com>
2025-04-10 19:47:24 -0600

    Añadido de funcionalidad a frmCierreDeCajas
    Se cambio el diseño a uno muy similar al de frmHistorial, donde ahora se envía la información de el endpoint hacia una lista con un buscador funcional

---
+++ DDW_PDV_WPF/Modelo/CierreCajasDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDW_PDV_WPF.Modelo
+{
+    public class CierreCajasDTO
+    {
+        public int idCierreCaja{ get; set; }
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+        public int idUsuario { get; set; }
+        public decimal TotalSistema { get; set; }
+        public decimal TotalFisico { get; set; }
+        public int idCaja { get; set; }
+
+
+        public decimal Diferencia => TotalFisico - TotalSistema;
+    }
+}
