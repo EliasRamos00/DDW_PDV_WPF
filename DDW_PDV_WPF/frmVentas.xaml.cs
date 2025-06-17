240d80d011cbcb6037e92e929b66c11ddda0d117
Erik Ricardo Campos Rosales <erikr.campos@gmail.com>
2025-06-16 23:59:56 -0600

    Hotfix eliminacion example.xaml
    Se elimino el example.xaml

--- DDW_PDV_WPF/frmVentas.xaml.cs
+++ DDW_PDV_WPF/frmVentas.xaml.cs
@@ -831,7 +831,7 @@ namespace DDW_PDV_WPF
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message);
+                System.Windows.Forms.MessageBox.Show("Error: " + ex.Message);
                 // Aquí manejar error de conexión
             }
         }
