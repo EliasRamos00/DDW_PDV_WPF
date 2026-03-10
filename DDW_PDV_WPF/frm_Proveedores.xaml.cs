3c0cb8731b16820ce883c989316da7881c70245e
AngelJavier10 <157035751+AngelJavier10@users.noreply.github.com>
2026-03-09 21:27:20 -0600

    FrmInventario
    cambios en el diseño del formulario de FrmInventario

--- DDW_PDV_WPF/frm_Proveedores.xaml.cs
+++ DDW_PDV_WPF/frm_Proveedores.xaml.cs
@@ -17,9 +17,9 @@ namespace DDW_PDV_WPF
     /// <summary>
     /// Lógica de interacción para frm_Proveedores.xaml
     /// </summary>
-    public partial class frm_Proveedores : Window
+    public partial class Frm_Proveedores : Window
     {
-        public frm_Proveedores()
+        public Frm_Proveedores()
         {
             InitializeComponent();
         }
